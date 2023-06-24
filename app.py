c3e66cf93333e019a6d414df445e67032340b1ba
nicoig <n.guerravillalobos@gmail.com>
2023-06-23 23:43:53 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,140 @@
+# Para crear el requirements.txt ejecutamos 
+# pipreqs --encoding=utf8 --force
+
+# Actualizar Repo de Github
+# git add .
+# git commit -m "Se actualizan las variables de entorno"
+# git push origin master
+
+# En Render
+# agregar en variables de entorno
+# PYTHON_VERSION = 3.9.12
+
+
+
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+import pickle
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from streamlit_chat import message
+import io
+import asyncio
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY") 
+
+# vectors = getDocEmbeds("gpt4.pdf")
+# qa = ChatVectorDBChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), vectors, return_source_documents=True)
+
+async def main():
+
+    async def storeDocEmbeds(file, filename):
+    
+        reader = PdfReader(file)
+        corpus = ''.join([p.extract_text() for p in reader.pages if p.extract_text()])
+        
+        splitter =  RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,)
+        chunks = splitter.split_text(corpus)
+        
+        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+        vectors = FAISS.from_texts(chunks, embeddings)
+        
+        with open(filename + ".pkl", "wb") as f:
+            pickle.dump(vectors, f)
+
+        
+    async def getDocEmbeds(file, filename):
+        
+        if not os.path.isfile(filename + ".pkl"):
+            await storeDocEmbeds(file, filename)
+        
+        with open(filename + ".pkl", "rb") as f:
+            global vectores
+            vectors = pickle.load(f)
+            
+        return vectors
+    
+
+    async def conversational_chat(query):
+        result = qa({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        # print("Log: ")
+        # print(st.session_state['history'])
+        return result["answer"]
+
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    chain = load_qa_chain(llm, chain_type="stuff")
+
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+
+    #Creating the chatbot interface
+
+
+    st.title("\U000026A1 BitBoosters \U000026A1")
+    st.title("PDF Chat")
+    st.markdown("<p style='color: white; font-size: 15px;'>Carga tus documentos PDF, realiza consultas en lenguaje natural y obtén respuestas sobre tu documento.</p>", unsafe_allow_html=True)
+
+
+    if 'ready' not in st.session_state:
+        st.session_state['ready'] = False
+
+    uploaded_file = st.file_uploader("Escoge un archivo", type="pdf")
+
+    if uploaded_file is not None:
+
+        with st.spinner("Procesando..."):
+        # Add your code here that needs to be executed
+            uploaded_file.seek(0)
+            file = uploaded_file.read()
+            # pdf = PyPDF2.PdfFileReader()
+            vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
+            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=vectors.as_retriever(), return_source_documents=True)
+
+        st.session_state['ready'] = True
+
+    st.divider()
+
+    if st.session_state['ready']:
+
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["¡Bienvenido! Ahora puede hacer cualquier pregunta sobre " + uploaded_file.name]
+
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ["Hey!"]
+
+        # container for chat history
+        response_container = st.container()
+
+        # container for text box
+        container = st.container()
+
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                user_input = st.text_input("Ingrese su solicitud:", placeholder="Ej: Resuma el trabajo en unas pocas oraciones", key='input')
+                submit_button = st.form_submit_button(label='Send')
+
+            if submit_button and user_input:
+                output = await conversational_chat(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
